cf33358db3ff3f043192e3f3c72718dc2c049df6
fshogf <Www.pfateme1381@gmail.com>
2025-05-25 08:45:00 +0330

    Add static messages

---
+++ utils/messages.py
@@ -0,0 +1,12 @@
+HELP_COMMAND_RESPONSE = """
+👋 Welcome! Here's how you can interact with this bot:
+
+🔹 /start - Start chatting with the bot  
+🔹 /time - Get the current time  
+🔹 /fact - Receive an interesting random fact  
+🔹 /joke - Get a random funny joke 😂  
+🔹 /date - See today’s date in the Persian (Jalali) calendar  
+🔹 /help - View this help message again
+
+🔍 Use inline mode: @Googleplay_review_bot <app name> to search for app details from Google Play.
+"""
